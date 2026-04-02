c43b81dfb60b329a02bad2978701b901c9bbb096
Abhishek Roy <abhishek8400roy@gmail.com>
2026-04-02 11:28:32 +0530

    text to speech ui cretaed

---
+++ src/app/(dashboard)/text-to-speech/layout.tsx
@@ -0,0 +1,12 @@
+import { TextToSpeechLayout } from "@/features/text-to-speech/views/text-to-speech-layout";
+
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+
+  return <TextToSpeechLayout>{children}</TextToSpeechLayout>
+};
